d351d37e3eb7edcf0d8bb42a1d45cfbc9668256d
mrspiggot <walker.richard.james@gmail.com>
2023-06-11 13:22:28 +0100

    added prompt_navigator.py

--- prompt_navigator.py
+++ prompt_navigator.py
@@ -114,8 +114,8 @@ class StrategyDeterminer:
 
         self.template = """Consider the following problem or puzzle: {question}. Based on the characteristics of the problem, identify the most suitable approach among the three techniques described below. consider each carefully in the context of the question, write out the likelihood of success of each, and then select the most appropriate approach:
 1. The solution for this problem requires searching for further information online, generating reasoning traces and task-specific actions in an interleaved manner. Starting with incomplete information this technique will prompt for the need to get additional helpful information at each step. It allows for dynamic reasoning to create, maintain, and adjust high-level plans for acting, while also interacting with external sources to incorporate additional information into reasoning [1].
-2. This technique revolves around exploring multiple reasoning paths over thoughts. It treats the problem as a search over a tree structure, with each node representing a partial solution and the branches corresponding to operators that modify the solution. It involves thought decomposition, thought generation, state evaluation, and a search algorithm [2].
-3. This technique focuses on generating a coherent series of intermediate reasoning steps that lead to the final answer. It mimics a step-by-step thought process similar to how humans solve complex problems. The approach provides interpretability, decomposes multi-step problems into intermediate steps, and allows for additional computation allocation [3].
+2. This problem is complex and the solution requires exploring multiple reasoning paths over thoughts. It treats the problem as a search over a tree structure, with each node representing a partial solution and the branches corresponding to operators that modify the solution. It involves thought decomposition, thought generation, state evaluation, and a search algorithm [2].
+3. This problem is simple and the solution may be obtained by focusing on generating a coherent series of reasoning steps that lead to the final answer. The approach provides interpretability, decomposes multi-step problems into intermediate steps, and allows for additional computation allocation [3].
 Based on the characteristics of the given problem or puzzle, select the technique that aligns most closely with the nature of the problem. It is important to first provide the number of the technique that best solves the problem, followed by a period. Then you may provide your reason why you have chosen this technique. 
 
 The number of the selected technique is...
@@ -130,7 +130,7 @@ The number of the selected technique is...
             return None
 
     def determine_and_execute(self):
-        llm_1 = OpenAI(temperature=0.9, max_tokens=3000)
+        llm_1 = OpenAI(temperature=0.6, max_tokens=3000)
         prompt = PromptTemplate(template=self.template, input_variables=["question"])
         llm_chain = LLMChain(prompt=prompt, llm=llm_1)
 
@@ -154,7 +154,7 @@ def run_app():
     icon = Image.open("Colour Logo.png")
     st.image(icon, width=100)
     st.title("Lucidate Prompt Strategy Demo. Powered by LangChain 🦜🔗 ")
-    question = st.text_area('Enter your question here:', height=400)
+    question = st.text_area('Enter your question here:', height=200)
 
     # run strategy determiner
     if question:
